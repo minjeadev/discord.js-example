ff7a6a4dc6f40cd93c8b5b27ee0e4768b18edb68
minjeadev <93039870+minjeadev@users.noreply.github.com>
2022-02-24 17:47:39 +0900

    Update index.js
    messageCreate!

--- sendMessage/index.js
+++ sendMessage/index.js
@@ -5,10 +5,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.on('message', message => {
+client.on('messageCreate', message => {
   if(message.content=="!ping"){
-    message.reply("pong!") // 답장 형식
-    // 답장 형식이 아니라 그냥 채널에 메시지만 보내고 싶으면,
-    // message.channel.send("pong")
+    message.reply("pong!") // reply
   }
 })
